e26e6573ca793361d441abf450661a2e6f5e777d
nkc20236035 <nkc23.etou.hiroto@gmail.com>
2025-02-14 14:31:47 +0900

    stageのいろいろ

--- DragDropAttack/Assets/Script/StageManager.cs
+++ DragDropAttack/Assets/Script/StageManager.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageManager : MonoBehaviour
 {
     private float timer = 0;
     public GameObject EnemyGene;
     public GameObject CommandGene;
+    //public Text text;
+    public Image image1;
+    public Image image2;
+    private bool isFading = true;
+    private float alpha = 0f;
+    private float fedeSpeed = 1.0f;
 
     void Start()
     {
-        
+        //text.text = "";
+        Color color1 = image1.color;
+        color1.a = 0f;
+        image1.color = color1;
+
+        Color color2 = image2.color;
+        color2.a = 0f;
+        image2.color = color2;
     }
 
     void Update()
@@ -18,8 +32,37 @@ public class StageManager : MonoBehaviour
         timer += Time.deltaTime;
         if(timer > 4)
         {
+            Color color = image2.color;
+            color.a = 0f;
+            image2.color = color;
             EnemyGene.SetActive(true);
             CommandGene.SetActive(true);
+        }
+        else if(timer >= 3)
+        {
+            Color color1 = image1.color;
+            color1.a = 0f;
+            image1.color = color1;
+            Color color2 = image2.color;
+            color2.a = 1f;
+            image2.color = color2;
+            //text.text = "スタート";
+        }
+        else if(timer >= 1)
+        {
+            if(isFading)
+            {
+                alpha += fedeSpeed * Time.deltaTime;
+                alpha = Mathf.Clamp01(alpha);
+                Color color1 = image1.color;
+                color1.a = alpha;
+                image1.color = color1;
+                if(alpha >= 1f)
+                {
+                    isFading = false;
+                }
+            }
+            //text.text = "バトル";
         }
 
     }
